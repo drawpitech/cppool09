9efde16ac0d9a93bf5b757fe810d620365e0a6b3
Clément <clement2104.boillot@gmail.com>
2024-01-23 14:05:43 +0100

    Try to fix task 6

--- IPotion.hpp
+++ IPotion.hpp
@@ -10,9 +10,10 @@
 class IPotion {
    public:
     IPotion(int power, int health) : _power(power), _health(health) {}
+    virtual ~IPotion() = default;
 
-    [[nodiscard]] int getPower() const { return _power; }
-    [[nodiscard]] int getHealth() const { return _health; }
+    [[nodiscard]] virtual int getPower() const { return _power; }
+    [[nodiscard]] virtual int getHealth() const { return _health; }
 
    private:
     int _power;
